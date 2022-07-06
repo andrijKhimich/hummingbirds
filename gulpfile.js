3762d6f2a3313f6950154b123024ab51c746ff1b
Andriy Khimich <andrykhimich@gmail.com.noreply.github.com>
2022-07-06 20:19:52 +0300

    deploy

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task("js", function () {
 
       // "src/libs/bootstrap-4.6.0/dist/js/bootstrap.min.js",
 
-      "src/js/script.js",
+      // "src/js/script.js",
     ])
     .pipe(sourcemaps.init())
     .pipe(babel())
